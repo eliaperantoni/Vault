c796bab8819321032d38c0b7e8ebfe930cd6f73c
hellix08 <hellix08.tubo@gmail.com>
2017-04-19 15:04:16 +0200

    Switched to SQLite, looking forward to use Hibernate ORM

--- src/com/extensys/vault/Server.java
+++ src/com/extensys/vault/Server.java
@@ -28,9 +28,7 @@ public class Server {
     }
     void connect(String databaseUsername, String databasePassword) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection
-                    ("jdbc:mysql://localhost:3306/vault", databaseUsername, databasePassword);
+            con = DriverManager.getConnection("jdbc:sqlite:vault.db");
         } catch (Exception e) {
             e.printStackTrace();
         }
